3d3a1628c35bef99fc43f35a4d3a817935e0228c
sanija manmitha <90247192+saniya2008sl@users.noreply.github.com>
2021-12-09 17:55:44 +0530

    Add files via upload

---
+++ 1.sh
@@ -0,0 +1,175 @@
+#!/bin/bash
+echo "insert text here" > kixerfT.txt
+echo "insert text here" > 1.txt
+echo "insert text here" > 2.txt
+echo "insert text here" > 3.txt
+echo "insert text here" > 4.txt
+echo "insert text here" > 5.txt
+echo "insert text here" > 6.txt
+echo "insert text here" > 7.txt
+echo "insert text here" > 8.txt
+echo "insert text here" > 9.txt
+echo "insert text here" > 10.txt
+echo "insert text here" > 11.txt
+echo "insert text here" > 12.txt
+echo "insert text here" > 13.txt
+echo "insert text here" > 14.txt
+echo "insert text here" > 15.txt
+echo "insert text here" > 16.txt
+echo "insert text here" > 17.txt
+echo "insert text here" > 18.txt
+echo "insert text here" > 19.txt
+echo "insert text here" > 20.txt
+echo "insert text here" > 21.txt
+echo "insert text here" > 22.txt
+echo "insert text here" > 23.txt
+echo "insert text here" > 24.txt
+echo "insert text here" > 25.txt
+echo "insert text here" > 26.txt
+echo "insert text here" > 27.txt
+echo "insert text here" > 28.txt
+echo "insert text here" > 29.txt
+echo "insert text here" > 30.txt
+echo "insert text here" > 31.txt
+echo "insert text here" > 32.txt
+echo "insert text here" > 33.txt
+echo "insert text here" > 34.txt
+echo "insert text here" > 35.txt
+echo "insert text here" > 36.txt
+echo "insert text here" > 37.txt
+echo "insert text here" > 38.txt
+echo "insert text here" > 39.txt
+echo "insert text here" > 40.txt
+echo "insert text here" > 41.txt
+echo "insert text here" > 42.txt
+echo "insert text here" > 43.txt
+echo "insert text here" > 44.txt
+echo "insert text here" > 45.txt
+echo "insert text here" > 46.txt
+echo "insert text here" > 47.txt
+echo "insert text here" > 48.txt
+echo "insert text here" > 49.txt
+echo "insert text here" > 50.txt
+echo "insert text here" > 51.txt
+echo "insert text here" > 52.txt
+echo "insert text here" > 53.txt
+echo "insert text here" > 54.txt
+echo "insert text here" > 55.txt
+echo "insert text here" > 56.txt
+echo "insert text here" > 57.txt
+echo "insert text here" > 58.txt
+echo "insert text here" > 59.txt
+echo "insert text here" > 60.txt
+echo "insert text here" > 61.txt
+echo "insert text here" > 62.txt
+echo "insert text here" > 63.txt
+echo "insert text here" > 64.txt
+echo "insert text here" > 65.txt
+echo "insert text here" > 66.txt
+echo "insert text here" > 67.txt
+echo "insert text here" > 68.txt
+echo "insert text here" > 69.txt
+echo "insert text here" > 70.txt
+printf "Hello, I'm kixerfT,
+Im Programmer & Hacker... ;
+                                              ************************************
+In information technology, a hacker is a person skilled in hacking,
+i.e. the unauthorized and often criminal use of or connection to information machines,
+i.e. computers or telephone systems, who use their technical knowledge to "hack,"
+or penetrate, compromise, and control, a computer or other technology in some way.
+Computer hacking and phone hacking are the two most known types of hacking.
+"Hack" and "hacking" are informal terms that has become highly useful, 
+its meaning ranging from the idea of masterful and adaptive building (building things is a high-level idea), 
+down to the deliberate corruption of the World's information infrastructure, in the service of criminals. 
+"Hacking" is thus controversial, as it is not so much an idea of great skill or mastery,
+but rather is an idea of accessing and tampering with information devices which are personal, 
+corporate telecommunications, or government property, either domestic or foreign in each above category.
+And then doing things with these devices are either minimally damaging in the short term, but may be damaging in the long term,
+or are maximally damaging in the short term, and therefore are a part of a larger criminal or military operation (i.e. to steal government or corporate secrets, to destroy their systems, steal financial objects, extort employees and citizens).
+Hackers a goal or overcome an obstacle, within a computerized system by non-standard means. 
+Though the term hacker has become associated in popular culture with a security hacker  someone who utilizes their technical know-how of bugs or exploits to break into computer systems and access data which would otherwise be unavailable to them  hacking can also be utilized by legitimate figures in legal situations. For example, 
+law enforcement agencies sometimes use hacking techniques in order to collect evidence on criminals and other malicious actors.
+This could include using anonymity tools (such as a VPN, or the dark web) to mask their identities online,
+posing as criminals themselves.[1][2] Likewise, covert world agencies can employ hacking techniques in the legal conduct of their work.
+Oppositely, hacking and cyber-attacks are used extra- and illegally by law enforcement and security agencies (conducting warrantless activities), and employed by State actors as a weapon of both legal and illegal warfare.
+ 
+ 
+ 
+                              ****** join with me ******
+
+ Yt = https://www.youtube.com/channel/UC96uODvt-mo0JQxe3X5aRSQ/featured
+ Github = https://github.com/saniya2008sl
+ %s.\n" $USER > kixerfT.txt
+sleep 5s
+rm 1.txt
+rm 2.txt
+rm 3.txt
+rm 4.txt
+rm 5.txt
+rm 6.txt
+rm 7.txt
+rm 8.txt
+rm 9.txt
+rm 10.txt
+rm 11.txt
+rm 12.txt
+rm 13.txt
+rm 14.txt
+rm 15.txt
+rm 16.txt
+rm 17.txt
+rm 18.txt
+rm 19.txt
+rm 20.txt
+sleep 3s
+rm 21.txt
+rm 22.txt
+rm 23.txt
+rm 24.txt
+rm 25.txt
+rm 26.txt
+rm 27.txt
+rm 28.txt
+rm 29.txt
+rm 30.txt
+rm 31.txt
+rm 32.txt
+rm 33.txt
+rm 34.txt
+rm 35.txt
+rm 36.txt
+rm 37.txt
+rm 38.txt
+rm 39.txt
+rm 40.txt
+rm 41.txt
+rm 42.txt
+rm 43.txt
+rm 44.txt
+rm 45.txt
+rm 46.txt
+rm 47.txt
+rm 48.txt
+rm 49.txt
+rm 50.txt
+sleep 5s
+rm 51.txt
+rm 52.txt
+rm 53.txt
+rm 54.txt
+rm 55.txt
+rm 56.txt
+rm 57.txt
+rm 58.txt
+rm 59.txt
+rm 60.txt
+rm 61.txt
+rm 62.txt
+rm 63.txt
+rm 64.txt
+rm 65.txt
+rm 66.txt
+rm 67.txt
+rm 68.txt
+rm 69.txt
+rm 70.txt
